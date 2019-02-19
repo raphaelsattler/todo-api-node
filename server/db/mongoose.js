fae62cc9b5c1f78f73b7d1d33225561f9b3aefaa
Raphael Sattler <raphaelsattler@hotmail.com>
2019-02-19 20:19:15 -0300

    Add POST /todos route and refactor mongoose

---
+++ server/db/mongoose.js
@@ -0,0 +1,8 @@
+const uri = 'mongodb://raphael_sattler:samsung7s@node-course-shard-00-00-dhztq.mongodb.net:27017,node-course-shard-00-01-dhztq.mongodb.net:27017,node-course-shard-00-02-dhztq.mongodb.net:27017/TodoApp?ssl=true&replicaSet=Node-course-shard-0&authSource=admin&retryWrites=true'
+
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(uri);
+
+module.exports = {mongoose};
